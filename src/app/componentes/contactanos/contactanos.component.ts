46d58580feb776fdccb35d8fd851de58186ef116
victor <victor.herrera.guzman@cftmail.cl>
2024-12-05 15:26:54 -0300

    prueba final

--- src/app/componentes/contactanos/contactanos.component.ts
+++ src/app/componentes/contactanos/contactanos.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { SolicitudFormularioService } from '../../servicios/solicitud-formulario/solicitud-formulario.service';
+import { TecnicosService } from '../../tecnicos.service'; 
+
 
 @Component({
   selector: 'app-contactanos',
@@ -10,7 +12,11 @@ import { SolicitudFormularioService } from '../../servicios/solicitud-formulario
 export class ContactanosComponent {
   formularioForm;
   datos_formulario: any;
-  constructor(private formBuild: FormBuilder, private solicitudFormularioSrv:SolicitudFormularioService){
+  tecnicos: any[] = [];  // Nueva variable para almacenar los técnicos
+  
+  constructor(private formBuild: FormBuilder, 
+              private solicitudFormularioSrv: SolicitudFormularioService,
+              private tecnicosService: TecnicosService) {  // Inyecta el servicio de técnicos
     this.formularioForm = this.formBuild.group({
       nombre: '',
       apellido: '',
@@ -19,21 +25,32 @@ export class ContactanosComponent {
       mensaje: ''
     });
   }
-  ngOnInit(){
-    
-  }
-  enviarDatos(){
-    this.solicitudFormularioSrv.registrarFormulario(this.formularioForm.value).subscribe(
-      (response:any) => {
-        
-        this.datos_formulario = response.solicitud_formulario
-        console.log(this.datos_formulario);        
-        alert("Datos guardados correctamente");
-        this.formularioForm.reset();
-      },error => {
-        console.log(error);
+
+  ngOnInit() {
+    // Obtener los técnicos cuando el componente se inicializa
+    this.tecnicosService.getTecnicos().subscribe(
+      (response: any) => {
+        this.tecnicos = response;  // Asigna los técnicos a la variable
+        console.log(this.tecnicos);  // Ver los datos de los técnicos
+      },
+      (error) => {
+        console.log(error);  // Manejo de errores
       }
-    )   
+    );
   }
 
+  enviarDatos() {
+    this.solicitudFormularioSrv.registrarFormulario(this.formularioForm.value).subscribe(
+      (response: any) => {
+        this.datos_formulario = response.solicitud_formulario;
+        console.log(this.datos_formulario);
+        alert("Datos guardados correctamente");
+        this.formularioForm.reset();
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
+
